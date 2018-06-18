984335514847d377cbc62ac0c02a44249cfee799
Tiem Song <tiem@google.com>
2018-06-18 16:13:59 -0700

    Add data binding

--- app/src/main/java/com/example/databindingkotlinsample/MainActivity.kt
+++ app/src/main/java/com/example/databindingkotlinsample/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.databindingkotlinsample
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.example.databindingkotlinsample.databinding.ActivityMainBinding
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -12,7 +14,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        binding.greeting = "Data Binding and Kotlin"
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
